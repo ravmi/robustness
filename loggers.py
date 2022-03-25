aa55b981641e34ca1eb4d337cd2ab8ff6397d5f0
Rafał Michaluk <rafal.michaluk@codility.com>
2022-03-25 18:24:50 +0100

    fix iterations

--- loggers.py
+++ loggers.py
@@ -1,12 +1,13 @@
 from clearml import Logger
+from collections import defaultdict
 
 class ClearMLLogger():
     def __init__(self, plot_name):
         self.log = Logger.current_logger()
         self.plot_name = plot_name
-        self.iteration = 0
-        # TODO fix this
+        self.iteration = defaultdict(lambda: 0)
 
     def report_scalar(self, plot_name, series_name, scalar):
-        self.log.report_scalar(plot_name, series_name, iteration=self.iteration, value=scalar)
-        self.iteration += 1
+        key = (plot_name, series_name)
+        self.log.report_scalar(plot_name, series_name, iteration=self.iteration[key], value=scalar)
+        self.iteration[key] += 1
